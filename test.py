b3e04aa30e02936333e5803cc9d2ab86b6a2bb6f
Robijn Van Houts <robijnvanhouts@robijns-mbp.home>
2021-03-05 10:10:51 +0100

    all

---
+++ test.py
@@ -0,0 +1,13 @@
+# import csv
+
+# with open('survey_results_public_mega_inc.csv', 'r') as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
+
+
+import csv
+with open('survey_results_public_mega_inc.csv', 'r') as file:
+    reader = csv.reader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    for row in reader:
+        print(row)
